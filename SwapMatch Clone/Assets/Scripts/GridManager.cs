11362c02fcf36c4712a3624894e74cee6238f340
Ronojoy Mitra <Ezioweasley123#>
2019-03-28 12:38:35 -0400

    Started working on the player object

--- SwapMatch Clone/Assets/Scripts/GridManager.cs
+++ SwapMatch Clone/Assets/Scripts/GridManager.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    public static GridManager Instance;
     const int ROWS = 7;
     const int COLUMNS = 5;
-    int[,] _gems = new int[COLUMNS, ROWS];
+    public int[,] _gems = new int[COLUMNS, ROWS];
+    Color[] _colors = new Color[6];
     public GameObject _gemPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instance = this;
+        SetColors();
+        InstantiateGems();
     }
 
     // Update is called once per frame
     void Update()
     {
-        InstantiateGems();
+        
     }
     public void InstantiateGems()
     {
@@ -27,7 +31,17 @@ public class GridManager : MonoBehaviour
             {
                 GameObject gem = Instantiate(_gemPrefab);
                 gem.transform.position = new Vector3(x, y);
+                gem.GetComponent<SpriteRenderer>().material.color = _colors[Random.Range(0,6)];
             }
         }
+    }
+    public void SetColors()
+    {
+        _colors[0] = Color.red;
+        _colors[1] = Color.blue;
+        _colors[2] = Color.green;
+        _colors[3] = Color.yellow;
+        _colors[4] = new Color(0,0,0);
+        _colors[5] = new Color(128, 0, 128);
     }
 }
---
+++ SwapMatch Clone/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private int[,] _Gems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject.Find("GridManager").GetComponent<GridManager>();
+        GridManager.Instance._gems[];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
