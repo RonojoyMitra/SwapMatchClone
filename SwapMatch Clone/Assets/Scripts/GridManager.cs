e7e3cc3deab90f38f427be293b275d2e50be273a
Ronojoy Mitra <Ezioweasley123#>
2019-03-28 10:19:21 -0400

    SwapMatch Beginning

---
+++ SwapMatch Clone/Assets/Scripts/GridManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    int[] _gems = new int[5];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
