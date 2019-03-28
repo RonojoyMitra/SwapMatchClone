de748eef54f9e7811456f2bde291d0257102f46e
Ronojoy Mitra <Ezioweasley123#>
2019-03-28 11:18:06 -0400

    Added Grid Generation Code

--- SwapMatch Clone/Assets/Scripts/GridManager.cs
+++ SwapMatch Clone/Assets/Scripts/GridManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    int[] _gems = new int[5];
+    const int ROWS = 7;
+    const int COLUMNS = 5;
+    int[,] _gems = new int[COLUMNS, ROWS];
+    public GameObject _gemPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,17 @@ public class GridManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        InstantiateGems();
+    }
+    public void InstantiateGems()
+    {
+        for (int x = 0; x < COLUMNS; x++)
+        {
+            for (int y = 0; y < ROWS; y++)
+            {
+                GameObject gem = Instantiate(_gemPrefab);
+                gem.transform.position = new Vector3(x, y);
+            }
+        }
     }
 }
