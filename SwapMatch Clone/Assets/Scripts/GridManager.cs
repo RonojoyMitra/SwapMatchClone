14b47cc123be389de34f1b6cc6eb2c9eb559e31d
Ronojoy Mitra <Ezioweasley123#>
2019-04-11 11:50:32 -0400

    Fixed Gem Array being Null error

--- SwapMatch Clone/Assets/Scripts/GridManager.cs
+++ SwapMatch Clone/Assets/Scripts/GridManager.cs
@@ -23,20 +23,20 @@ public class GridManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int x = 0; x < COLUMNS; x++)
-        {
-            for (int y = 0; y < ROWS; y++)
-            {
-                if (x == PlayerScript.Instance.xposition && y == PlayerScript.Instance.yposition)
-                {
-                   tempColor = _gems[x, y].GetComponent<SpriteRenderer>().material.color;
-                   Destroy(_gems[x, y]);
-                   GameObject gem = Instantiate(_gemPrefab);
-                   gem.transform.position = new Vector3(PlayerScript.Instance.oldxposition, PlayerScript.Instance.oldyposition);
-                   gem.GetComponent<SpriteRenderer>().material.color = tempColor;
-                }
-            }
-        }
+        //for (int x = 0; x < COLUMNS; x++)
+        //{
+        //    for (int y = 0; y < ROWS; y++)
+        //    {
+        //        if (x == PlayerScript.Instance.xposition && y == PlayerScript.Instance.yposition && _gems[x,y] != null)
+        //        {
+        //           tempColor = _gems[x, y].GetComponent<SpriteRenderer>().material.color;
+        //           Destroy(_gems[x, y]);
+        //           GameObject gem = Instantiate(_gemPrefab);
+        //            gem.transform.position = PlayerScript.Instance.oldposition;
+        //           gem.GetComponent<SpriteRenderer>().material.color = tempColor;
+        //        }
+        //    }
+        //}
     }
     public void InstantiateGems()
     {
@@ -66,5 +66,22 @@ public class GridManager : MonoBehaviour
         _colors[3] = Color.yellow;
         _colors[4] = new Color(0,0,0);
         _colors[5] = new Color(128, 0, 128);
+    }
+    public void PlayerMovement()
+    {
+        if (_gems[PlayerScript.Instance.xposition, PlayerScript.Instance.yposition] != null)
+        {
+            tempColor = _gems[PlayerScript.Instance.xposition, PlayerScript.Instance.yposition].GetComponent<SpriteRenderer>().material.color;
+            Destroy(_gems[PlayerScript.Instance.xposition, PlayerScript.Instance.yposition]);
+            GameObject gem = Instantiate(_gemPrefab);
+            gem.transform.position = PlayerScript.Instance.oldposition;
+            _gems[(int)PlayerScript.Instance.oldposition.x,(int)PlayerScript.Instance.oldposition.y] = gem;
+            gem.GetComponent<SpriteRenderer>().material.color = tempColor;
+            Debug.Log("Gem isn't null");
+        }
+        else
+        {
+            Debug.Log("Gem is null");
+        }
     }
 }
--- SwapMatch Clone/Assets/Scripts/PlayerScript.cs
+++ SwapMatch Clone/Assets/Scripts/PlayerScript.cs
@@ -1,58 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
     public static PlayerScript Instance;
     //private int[,] _Gems = new int[7,5];
     public GameObject _playerPrefab;
+    GameObject _currentPlayer;
+    GameObject _oldPlayer;
+    public GameObject GridManager;
     public int xposition = 2;
     public int yposition = 3;
-    public int oldxposition = 2;
-    public int oldyposition = 3;
+    public Vector2 oldposition = new Vector2(2, 3);
+    public int _playerMoveCount = 6;
+    public Text _playerMoveText;
+    //public int oldxposition = 2;
+    //public int oldyposition = 3;
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
         //GameObject.Find("GridManager").GetComponent<GridManager>();
         //GridManager.Instance._gems[3, 2] = Instantiate(_playerPrefab);
-        GameObject Player = Instantiate(_playerPrefab);
+        _currentPlayer = Instantiate(_playerPrefab);
         //Player.transform.position = GridManager.Instance._gems[2,3];
         //GridManager.Instance._gems[3, 2] = Player;
-        Player.transform.position = new Vector3(xposition,yposition);
+        _currentPlayer.transform.position = new Vector3(xposition,yposition);
+        _playerMoveText.transform.position = _currentPlayer.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && xposition > 0)
         {
-            oldxposition = xposition;
+            oldposition = new Vector2(xposition, yposition);
             xposition--;
-            GameObject Player = Instantiate(_playerPrefab);
-            Player.transform.position = new Vector3(xposition ,yposition);
+            _currentPlayer.transform.position = new Vector3(xposition ,yposition);
+            _playerMoveText.transform.position = _currentPlayer.transform.position;
+            _playerMoveCount--;
+            GridManager.SendMessage("PlayerMovement"); 
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && xposition < 4)
         {
-            oldxposition = xposition;
+            //_oldPlayer = _currentPlayer;
+            oldposition = new Vector2(xposition, yposition);
             xposition++;
-            GameObject Player = Instantiate(_playerPrefab);
-            Player.transform.position = new Vector3(xposition ,yposition);
+            //_currentPlayer = Instantiate(_playerPrefab);
+            _currentPlayer.transform.position = new Vector3(xposition ,yposition);
+            _playerMoveText.transform.position = _currentPlayer.transform.position;
+            _playerMoveCount--;
+            GridManager.SendMessage("PlayerMovement");
+            //Destroy(_oldPlayer);
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && yposition < 6)
         {
-            oldyposition = yposition;
+            //_oldPlayer = _currentPlayer;
+            //oldyposition = yposition;
+            //oldxposition = xposition;
+            oldposition = new Vector2(xposition,yposition);
             yposition++;
-            GameObject Player = Instantiate(_playerPrefab);
-            Player.transform.position = new Vector3(xposition, yposition);
+            //_currentPlayer = Instantiate(_playerPrefab);
+            _currentPlayer.transform.position = new Vector3(xposition, yposition);
+            _playerMoveText.transform.position = _currentPlayer.transform.position;
+            _playerMoveCount--;
+            GridManager.SendMessage("PlayerMovement");
+            //Destroy(_oldPlayer);
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && yposition > 0)
         {
-            oldyposition = yposition;
+            //_oldPlayer = _currentPlayer;
+            oldposition = new Vector2(xposition,yposition);
             yposition--;
-            GameObject Player = Instantiate(_playerPrefab);
-            Player.transform.position = new Vector3(xposition, yposition + 1);
+            //_currentPlayer = Instantiate(_playerPrefab);
+            _currentPlayer.transform.position = new Vector3(xposition, yposition);
+            _playerMoveText.transform.position = _currentPlayer.transform.position;
+            _playerMoveCount--;
+            GridManager.SendMessage("PlayerMovement");
+            //Destroy(_oldPlayer);
         }
     }
 }
